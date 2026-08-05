944c7e0da3de19f9b932a110d86e83402f691814
Andrey Leonchikov <andreil499@gmail.com>
2026-08-05 23:56:24 +0200

    Revert

--- deps/wxInspector/wxInspector.cmake
+++ deps/wxInspector/wxInspector.cmake
@@ -1,7 +1,7 @@
 orcaslicer_add_cmake_project(
     wxInspector
     URL https://github.com/Noisyfox/wxInspector/archive/refs/tags/v1.0.0.zip
-    URL_HASH SHA256=96ffd6cc47898dd8147aab53d7d1b1911b507d9dbaecd5613ca2649468afd8b6
+    URL_HASH SHA256=0ba163956f2d468b19a91b96c5aba66ee9610843ea41dda628ea44cdafde7db7
     DEPENDS ${WXWIDGETS_PKG}
     CMAKE_ARGS
         -DCMAKE_CXX_FLAGS="-DwxDEBUG_LEVEL=0"
